b400e996ae99083fae634abacff6f8dacb410678
parfait2 <parkyl0208@naver.com>
2023-10-09 21:03:40 +0900

    [Update] Naver 검색 API 적용

--- src/main/java/com/example/server/controller/ServerApiController.java
+++ src/main/java/com/example/server/controller/ServerApiController.java
@@ -3,12 +3,54 @@ package com.example.server.controller;
 import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    // https://openapi.naver.com/v1/search/blog.xml
+    // ?query=%EA%B9%80%ED%95%B4%EC%A4%80
+    // &display=10
+    // &start=1
+    // &sort=sim
+    @GetMapping("/naver")
+    public String naver() {
+
+        String query = "김해준";
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/blog.xml")
+                .queryParam("query", query)
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "sim")
+                .encode(StandardCharsets.UTF_8)
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id","{id}") // 해당하는 값 넣기
+                .header("X-Naver-Client-Secret","{secret key}") // 해당하는 값 넣기
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
+    }
+
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age) {
         User user = new User();
